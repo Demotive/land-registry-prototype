231f48183769ca6f7a0e2cae48254d8be0b92a0a
Demotive <mat@demotive.com>
2014-04-03 17:09:49 +0100

    Another start page, initial results page

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,3 +1,23 @@
 $(function() {
+  
   $('#tabs').tabs();
+
+  $('.help-inner').hide();
+  $('.help .secondary').on('click', function(e) {
+    e.preventDefault();
+    $('.help-inner').toggle();
+  });
+
+  $('#resultsGoMapView').on('click', function(e) {
+    e.preventDefault();
+    $('a[href="#tabs-2"]').click();
+  });
+
+  /*var map = L.map('map').setView([51.505, -0.09], 1);
+  L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+  }).addTo(map);*/
+
+
 });
